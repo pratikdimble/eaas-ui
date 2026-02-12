3e4ff34a95c2ba71dad0f06b06a7e8cd7e21623f
pratikdimble <pratikdimble2238@gmail.com>
2026-02-12 19:26:03 +0530

    configure CD

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,9 +1,9 @@
 /***
  * Local Env
  */
-export const API_BASE_URL = 'http://localhost:8080/api'
+//export const API_BASE_URL = 'http://localhost:8080/api'
 
 /***
  * Production Env
  */
-// export const API_BASE_URL = 'https://evaluation-service-api-production.up.railway.app/api'
+export const API_BASE_URL = 'https://evaluation-service-api-production.up.railway.app/api'
